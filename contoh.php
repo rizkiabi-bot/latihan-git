8ea7701fb6a34e130dcb37d23492a02852cc3333
WP2_RizkiAbiNurhasan <84903151+rizkiabi-bot@users.noreply.github.com>
2021-09-20 12:26:32 +0700

    Add files via upload

---
+++ contoh.php
@@ -0,0 +1,8 @@
+<?php
+class contoh extends CI_Controller 
+{
+   public function index()
+   {echo "HAllo nama saya Rizki Abi Nurhasan<br>";
+    echo "Saya seorang mahasiswa S1 jurusan Sistem Informasi";
+   }
+}
