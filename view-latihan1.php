a8e55a3505073cfccd143d31b725c3cf9155004e
WP2_RizkiAbiNurhasan <84903151+rizkiabi-bot@users.noreply.github.com>
2021-09-26 22:59:55 +0700

    Add files via upload

---
+++ view-latihan1.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+
+</head>
+
+<body>
+    Halo kawan... yuk kita belajar web programing..!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini hasil pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
