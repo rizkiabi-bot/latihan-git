a7768d1366b8e9f7e42550bf6b8f968e29a92ed4
WP2_RizkiAbiNurhasan <84903151+rizkiabi-bot@users.noreply.github.com>
2021-10-03 09:58:10 +0700

    Add files via upload

---
+++ v_footer.php
@@ -0,0 +1,11 @@
+        <footer>
+        <nav>
+            <ul>
+                <li><a href="http://www.RentalBuku.com">Rental Buku</a></li>
+                <li><a href="https://www.youtube.com/channel/UCxCDtaB2O_lWka52mcwN-0Q">kiby Channel</a></li>
+            </ul>
+        </nav>
+        </footer>
+    </div>
+</body>
+</html>
